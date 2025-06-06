738f05456102cecd4dbe49108d1981f238fc8f53
j-jackson9 <jordancjack.20@gmail.com>
2025-06-05 21:20:27 -0500

    working on header queries

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-  function toggleHeart(heart) {
-        if (heart.classList.contains('fa-regular')) {
-            heart.classList.remove('fa-regular');
-            heart.classList.add('fa-solid');
-            heart.style.color = 'red';
-        } else {
-            heart.classList.remove('fa-solid');
-            heart.classList.add('fa-regular');
-            heart.style.color = 'black';
-        }
+function toggleHeart(heart) {
+    if (heart.classList.contains('fa-regular')) {
+        heart.classList.remove('fa-regular');
+        heart.classList.add('fa-solid');
+        heart.style.color = 'red';
+    } else {
+        heart.classList.remove('fa-solid');
+        heart.classList.add('fa-regular');
+        heart.style.color = 'black';
+    }
 
-        let likeText = heart.parentElement.querySelector('.like-text');
+    let likeText = heart.parentElement.querySelector('.like-text');
 
-        if (heart.classList.contains('fa-solid')) {
-            likeText.textContent = 'Unlike';
-        } else {
-            likeText.textContent = 'Like';
-        }
+    if (heart.classList.contains('fa-solid')) {
+        likeText.textContent = 'Unlike';
+    } else {
+        likeText.textContent = 'Like';
+    }
   }
+
+let sideBarButton = document.querySelector('.fa-bars');
+let sideBar = document.querySelector('.sidebar');
+
+  function openSideBar() {
+    if(window.innerWidth <= 768) {
+     if (sideBar.style.display === 'block') {
+        sideBar.style.display = 'none';
+    } else {
+        sideBar.style.display = 'block';
+    }
+  }
+}
