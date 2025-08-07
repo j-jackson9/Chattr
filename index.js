e264705b66d5e8a9f38fa785711793c6321704ae
j-jackson9 <jordancjack.20@gmail.com>
2025-08-06 21:20:21 -0500

    on the messages page, i added a feature where you select a conversation a mock message shows up

--- index.js
+++ index.js
@@ -41,12 +41,58 @@ let overLay = document.querySelector('.overlay');
 /*conversation function*/
 
 function selectConversation(name) {
-   let convoList = document.querySelector('.convo');
-   let message = document.querySelector('.message-content');
-   if(convoList.textContent.name === name) {
-       message.innerHTML
+    localStorage.setItem('selectedConversation', name);
+
+    const messageContent = document.querySelector('.message-content');
+
+  if (name === 'Tony Soprano') {
+    messages = `
+      <h1>Tony Soprano</h1>
+      <div class="chat-history">
+          <p class="message-in">Hey, how's it going?</p>
+          <p class="message-out">Not bad, just working on some stuff. You?</p>
+      </div>
+    `;
+  } else if (name === 'Ricky Montana') {
+    messages = `
+      <h1>Ricky Montana</h1>
+      <div class="chat-history">
+          <p class="message-in">Yo, you coming to the game?</p>
+          <p class="message-out">Yeah, I’ll be there in 10.</p>
+      </div>
+    `;
+  }
+    else if (name === 'Nick Johnson') {
+        messages = `
+        <h1>Nick Johnson</h1>
+        <div class="chat-history">
+            <p class="message-in">Hey, did you finish the report?</p>
+            <p class="message-out">I am NOT bailing you out.</p>
+        </div>
+    `;
+  } else if (name === 'Jazz James') {
+    messages = `
+      <h1>Jazz James</h1>
+      <div class="chat-history">
+          <p class="message-in">Hey, are we still on for tomorrow?</p>
+          <p class="message-out">Nah I am hanging with Ricky, sorry.</p>
+      </div>
+    `;
+  }
+
+  // Replace message content
+  messageContent.innerHTML = `
+    ${messages}
+    <form class="message-input">
+        <input type="text" placeholder="Type a message...">
+        <button type="submit"><i class="fa-solid fa-arrow-up"></i></button>
+    </form>
+  `;
+} 
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedName = localStorage.getItem('selectedConversation');
+    if (savedName) {
+      selectConversation(savedName);
     }
-      
-   }
-
-
+  });
